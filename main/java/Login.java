d42c239144d38e7b8b1e602be710c6b94d6286e5
gillie04th <theophile.gillier@gmail.com>
2023-01-23 09:16:56 +0100

    modifications servlets et jsp

--- main/java/Login.java
+++ main/java/Login.java
@@ -1,11 +1,14 @@
 
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
 
 /**
  * Servlet implementation class Login
@@ -13,6 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/login")
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	static Logger log = Logger.getLogger(Login.class.getName());
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -29,6 +33,7 @@ public class Login extends HttpServlet {
 
 		request.setAttribute("title", "Connexion");
 		request.setAttribute("joueur", "toto");
+		request.setAttribute("wrongCredential", request.getAttribute("wrongCredential"));
 		
 		this.getServletContext().getRequestDispatcher("/WEB-INF/jsp/login.jsp").forward(request, response);
 	}
@@ -37,8 +42,21 @@ public class Login extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		// doGet(request, response);
+		
+		//System.out.println(request.getParameter("email") + " " + request.getParameter("password") + " " + request.getContextPath() + "/home");
+		
+		if(request.getParameter("email") != "" && request.getParameter("password") != "") {
+			
+			request.getSession().setAttribute("joueur", request.getParameter("email"));
+			
+			response.sendRedirect(request.getContextPath() + "/home");
+		}else {
+			log.debug("Hello this is a debug message");
+			log.info("Hello this is an info message");
+			request.setAttribute("wrongCredential", true);
+			doGet(request, response);
+		}
 	}
 
 }
